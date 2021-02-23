7e3292e166115c1aae0fd222dfcd3f4245e3ba55
s0ngkran <jinnawat8@gmail.com>
2021-02-23 20:07:51 +0700

    first

---
+++ handapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    choice_sex = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+    sex = models.CharField(max_length=255, choices=choice_sex, null=True, blank=True)
+    height = models.IntegerField(default=0, null=True, blank=True)
+    weight = models.IntegerField(default=0, null=True, blank=True)
+    
+    
---
+++ handapp/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from hand_collection.utils import *
+from .forms import *
+from django.contrib.auth import authenticate, login, logout
+
+class RegisterPage(MyView):
+    template_name = 'index/register.html'
+    form_user = UserForm
+    form_profile = ProfileForm
+    def get(self, request, *args, **kwargs):
+        self.context.update({
+            'form_user': self.form_user,
+            'form_profile': self.form_profile,
+        }) 
+        return self.render(request)
+    def post(self, request, *args, **kwargs):
+        data = request.POST.copy()
+        act = data.get('act')
+        if act == 'register':
+            form1 = self.form_user(data)
+            form2 = self.form_profile(data)
+            if form1.is_valid and form2.is_valid:
+                new_user = User.objects.create_user(
+                    username = data['username'],
+                    password = data['password'],
+                )
+                new_user.save()
+                new_profile = form2.save(commit=False)
+                new_profile.user = new_user
+                new_profile.save()
+                
+        user = authenticate(username=data['username'], password=data['password'])
+        if user is not None:
+            login(request, user)
+            return redirect('index-page')
+        
+        return redirect('login-page')
+    
+class LoginPage(MyView):
+    template_name = 'index/login.html'
+    def get(self, request, *args, **kwargs):
+        self.context.update({
+            'users': User.objects.all(),
+            'profiles': Profile.objects.all(),
+        })
+        if request.user.is_authenticated:
+            return redirect('index-page')
+        return self.render(request)
+    
+    def post(self, request, *args, **kwargs):
+        data = request.POST.copy()
+        user = authenticate(username=data['username'], password=data['password'])
+        if user is not None:
+            login(request, user)
+            return redirect('index-page')
+        
+
+class LogoutPage(MyView):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('login-page')
+        
+class IndexPage(MyView):
+    template_name = 'index/index.html'
+    permission = 9999
+    
+    @has_perm
+    def get(self, request, *args, **kwargs):
+        return self.render(request)
+    
+
+    
